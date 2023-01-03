13b62ff00ac22c5c624977a666d497a98451ecec
ahsansiddiqui2 <ahsansiddiqui2@users.noreply.github.com>
2023-01-03 09:04:33 +0500

    a

--- DSALab/Lab4.java
+++ DSALab/Lab4.java
@@ -33,7 +33,40 @@ class Lab4{
 	public static void InsertionSort2D(int [][] arr){
 
 	}
-	public static void QuickSort1D(int[] arr){}
+			static int partition(int[] arr, int low, int high)
+	{
+	
+	int pivot = arr[high];
+	
+	int i = (low - 1);
+
+for (int j = low; j <= high - 1; j++) {
+
+if (arr[j] < pivot) {
+
+i++;
+swap(arr, i, j);
+}
+}
+swap(arr, i + 1, high);
+return (i + 1);
+}
+
+static void quickSort(int[] arr, int low, int high)
+{
+if (low < high) {
+
+int pi = partition(arr, low, high);
+
+quickSort(arr, low, pi - 1);
+quickSort(arr, pi + 1, high);
+	}
+
+ 	}
+	
+public static void QuickSort1D(int [] arr){
+	
+}
 	public static void QuickSort2D(int [] [] arr){}
 
 	public void Run(int[] arr1d,int [][] arr2d){
