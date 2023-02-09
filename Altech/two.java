97606d30266164fbc049a687b2120ef461741b54
Muhammad Ahsan <mahsansiddiqui2@gmail.com>
2023-02-09 08:41:41 +0500

    Update two.java

--- Altech/two.java
+++ Altech/two.java
@@ -1,34 +1,19 @@
-
-import java.util.Arrays;
-/**
- * two
- */
 public class two {
-
     public static void main(String[] args) {
-        reverse(args);
-
+        reverse(args[0]);
     }
-    public static void reverse(String[] a){
-        char[] c = a[0].toCharArray();
+       public static void reverse(String a){
+        char[] c = a.toCharArray();
         for(int i=0,j=c.length-1;i<j;i++,--j){
-            
-            if(c[i]=='0'){
-                continue;
-                
-            }
-            else{
                 char temp = c[i];
             c[i] = c[j];
             c[j] = temp;
-
             }
+        String w ="";
+        for(int i=0;i<c.length;i++){
+            w+=c[i];
         }
-        StringBuffer w = new StringBuffer();
-        for(int i=0;i<c.length-1;i++){
-            if(c[i+1]==)
-            w.append(q);
-        }
-        System.out.println(w);
+        int o = Integer.parseInt(w);
+        System.out.println(o);
     }
 }
